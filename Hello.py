6480fa298df2efdedf68a4e4a5a248a67aab5b22
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2018-12-14 18:09:29 +0530

    Create Hello.py
    output - 
    ['Sheldon Cooper']

---
+++ Hello.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+a = tf.constant("Sheldon ")
+b = tf.constant(["Cooper"])
+c = tf.add(a,b)
+session = tf.Session()
+result = session.run(c)
+print(result)
+session.close();
