1decfd662af55342776e0ca282da37e946cbd9df
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2018-12-16 23:31:07 +0530

    Update [Grade Predictor].py

--- SciKitLearn/[Grade Predictor].py
+++ SciKitLearn/[Grade Predictor].py
@@ -1,16 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[26]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow
-
-
-# In[25]:
-
 
 marks= np.array([[35],[46],[83],[55],[57],[92],[74],[77]])
 grade = np.array([4,5,9,6,6,10,8,8])
@@ -24,10 +14,6 @@ plt.scatter(marks,grade)
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(real, pred)
 print(accuracy*100)
-
-
-# In[ ]:
-
 
 
 
