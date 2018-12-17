0a5c225488af5cb9454f50dde7fad627169fbc18
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2018-12-17 14:39:46 +0530

    Update [Grade Predictor].py

--- SciKitLearn/[Grade Predictor].py
+++ SciKitLearn/[Grade Predictor].py
@@ -1,19 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow
 
-marks= np.array([[35],[46],[83],[55],[57],[92],[74],[77]])
-grade = np.array([4,5,9,6,6,10,8,8])
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+marks= np.array([[35],[46],[57],[92],[74],[77]])
+grade = np.array([4,5,6,10,8,8])
+from sklearn import linear_model
+clf = linear_model.LinearRegression()
 clf.fit(marks,grade)
 pred = clf.predict([[40],[98]])
 real = [[6],[10]]
-print(pred)
+print(np.ceil(pred)-1)
 plt.scatter(marks,grade)
+plt.plot([[40],[98]],np.ceil(pred)-1,'r')
 from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(real, pred)
-print(accuracy*100)
-
-
 
