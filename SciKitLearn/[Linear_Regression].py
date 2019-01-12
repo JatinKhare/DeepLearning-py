67e849f387843301c931a1a53568980141d5f5de
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2019-01-12 12:43:40 +0530

    Update [Linear_Regression].py

--- SciKitLearn/[Linear_Regression].py
+++ SciKitLearn/[Linear_Regression].py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('BlahBlah.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
