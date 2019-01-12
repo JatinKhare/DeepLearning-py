5a5b999fe021a4664269a6c326d3a4deba593359
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2019-01-12 17:17:24 +0530

    Update [Random_Forest].py

--- SciKitLearn/[Random_Forest].py
+++ SciKitLearn/[Random_Forest].py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('BlahBlah.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
@@ -26,7 +26,4 @@ X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
 plt.show()
